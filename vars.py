779f9b1e3342d68ac87255f66a0b31c3b456d0bf
Shittu-kumar <shittukumar37@gmail.com>
2025-12-10 22:28:38 +0530

    Update AUTH_USERS to a new user ID

--- vars.py
+++ vars.py
@@ -6,4 +6,4 @@ from os import environ
 API_ID = int(environ.get("API_ID", "20114039"))
 API_HASH = environ.get("API_HASH", "87297b8f3cc8fc9bbce591ad30da5896")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7676633301:AAH8UB88XA5JgQRXR-GWLyfdDTpvEsrjVbU")
-AUTH_USERS = "8172163893"
+AUTH_USERS = "8420375549"
